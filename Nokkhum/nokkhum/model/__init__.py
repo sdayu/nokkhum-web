40818c91ee3563321499ec58b982c16e5fbf2992
Thanathip Limna <boatkrap@gmail.com>
2011-11-09 14:32:57 +0700

    try to remove

--- Nokkhum/nokkhum/model/__init__.py
+++
@@ -1,9 +0,0 @@
-from users import User, Group
-from cameras import Camera, CameraModel, Manufactory
-from image_processors import ImageProcessor
-from compute_node import CPUInfomation, MemoryInfomation, ComputeNode
-
-from mongoengine import connect
-
-def initial(setting):
-    connect(setting.get('mongodb.name'), host=setting.get('mongodb.url'))
