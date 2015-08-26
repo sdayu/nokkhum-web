55ac5669caefbb4e0f7d9b71aafab2818c20fe89
Thanathip Limna <boatkrap@gmail.com>
2015-08-26 13:25:47 +0700

    chage command log list

--- nokkhumweb/views/admin/command_log.py
+++ nokkhumweb/views/admin/command_log.py
@@ -2,9 +2,11 @@ from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
 from pyramid.response import Response
 from pyramid.security import authenticated_userid
+import datetime
 
 @view_config(route_name='admin.command_log.list', permission='role:admin', renderer='/admin/command_log/list.jinja2')
 def list(request):
     return dict(
+                datetime=datetime,
                 processor_commands = request.nokkhum_client.admin.processor_commands.list()
                 )
